14e6a32f75cc4e1f54c2185ade62d8dc4dfe8326
sukepio <naoto927sukena@gmail.com>
2020-12-24 01:40:14 +0000

    [Add] PostComment

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_23_132712) do
+ActiveRecord::Schema.define(version: 2020_12_24_012427) do
+
+  create_table "post_comments", force: :cascade do |t|
+    t.text "comment"
+    t.integer "user_id"
+    t.string "post_image_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "post_images", force: :cascade do |t|
     t.text "shop_name"
